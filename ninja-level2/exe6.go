14ab8d09fc6ee6eae3f566b44d82015883c4fb8f
tranthanh <tranvanthanhsd0@gmail.com>
2021-03-03 21:42:54 +0700

    docs: tim hieu co ban ve iota va cach thuc hoat dong

---
+++ ninja-level2/exe6.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+//Cách thức hoạt động của iota: iota tăng 1 sau mỗi dòng ngoại trừ dòng trống và chú thích.
+
+const (
+	a = 2017 + iota
+	b = 2017 + iota
+	c = 2017 + iota
+	d = 2017 + iota
+)
+
+func main() {
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+}
