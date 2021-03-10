4f4514967265952eabb8d4cd7b447b7b3885efa4
tranthanh <tranvanthanhsd0@gmail.com>
2021-03-10 21:04:06 +0700

    feat:em chua hieu ve dong thu 11

--- ninja-level3/exe2.go
+++ ninja-level3/exe2.go
@@ -8,7 +8,7 @@ func main() {
 	for i := 70; i <= 100; i++ {
 		fmt.Println(i)
 		for j := 0; j < 3; j++ {
-			fmt.Printf("\t%#U\n", i)
+			fmt.Printf("\t%#U\n", i) // %#U nó là như thế nào vậy anh. Em cho i chạy khác ở web. Trên web cho i := 60; i <= 90
 		}
 	}
 }
