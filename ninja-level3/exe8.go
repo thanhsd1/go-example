aa28c4ad35d3e3a68dfc71f3ae7b80d4b9847455
tranthanh <tranvanthanhsd0@gmail.com>
2021-03-11 21:18:17 +0700

    feat: gan gia tri de chay vong lap

---
+++ ninja-level3/exe8.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() { // Cấu trúc swtich case chạy vòng lặp
+	x := 4
+	switch x {
+	case 1:
+		fmt.Println("a")
+	case 2:
+		fmt.Println("b")
+	case 3:
+		fmt.Println("c")
+	case 4:
+		fmt.Println("d")
+	case 5:
+		fmt.Println("e")
+	}
+}
