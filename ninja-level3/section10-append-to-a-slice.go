684deab9bd8408bbece8e84e29fd2723e0d5f805
tranthanh <tranvanthanhsd0@gmail.com>
2021-03-14 15:34:23 +0700

    feat:delete append

--- ninja-level3/section10-append-to-a-slice.go
+++ ninja-level3/section10-append-to-a-slice.go
@@ -5,11 +5,21 @@ import "fmt"
 func main() {
 	x := []int{1, 3, 4, 6, 8}
 	fmt.Println(x)
+
 	x = append(x, 10, 12, 14, 16)
 	fmt.Println(x)
 
 	y := []int{20, 22, 24, 26, 28}
 	x = append(x, y...)
+	fmt.Println(x)
+
+	x = append(x[1:]) // delete append
+	fmt.Println(x)
+
+	x = append(x[2:]) // delete append
+	fmt.Println(x)
+
+	x = append(x[1:], x[2:]...) // delete append
 	fmt.Println(x)
 
 }
