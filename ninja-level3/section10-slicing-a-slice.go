67cb9efac16e11104fa666f7bba94e7ac3859f3c
tranthanh <tranvanthanhsd0@gmail.com>
2021-03-14 14:04:03 +0700

    feat:add code

--- ninja-level3/section10-slicing-a-slice.go
+++ ninja-level3/section10-slicing-a-slice.go
@@ -4,7 +4,8 @@ import "fmt"
 
 func main() {
 	x := []int{5, 4, 3, 1, 8}
-	fmt.Println(x[1])  // lấy ra 1 phần tử trong mảng
-	fmt.Println(x)     // in ra toàn bộ mảng của biên x
-	fmt.Println(x[2:]) // xoá đi  phần tử trong mảng. Hàm hiện tại là xoá đi 2 phần tử đếm từ trái sang phải. VD: "[2:]" ngược lại thì "[:2]" xoá từ phải sang trái
+	fmt.Println(x[1]) // lấy ra 1 phần tử trong mảng
+	fmt.Println(x)
+	fmt.Println(x[1:])
+	fmt.Println(x[1:5]) //  ở đây có nghĩa là phần tử sau trừ cho phần tử trước. Đúng không vậy anh??
 }
