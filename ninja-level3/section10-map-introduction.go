db78cfd62ee62285303845191dc70cce9fea9f4e
tranthanh <tranvanthanhsd0@gmail.com>
2021-03-16 20:11:55 +0700

    feat:ham map

--- ninja-level3/section10-map-introduction.go
+++ ninja-level3/section10-map-introduction.go
@@ -3,11 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	x := map[string]int{
+	x := map[string]int{ // Cách để khai báo kiểu chữ và kiểu số trong hàm map
 
 		"Van Thanh":  97,
 		"Tran Thanh": 79,
 	}
 	fmt.Println(x)
+
+	fmt.Println(x["Van Thanh"])
+
+	fmt.Println(x["Van Thanh1"]) // Giá trị này sẽ = 0 vì chưa được khai báo
+
+	fmt.Println(x["Tran Thanh"])
 
 }
