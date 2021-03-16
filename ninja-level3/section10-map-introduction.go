4b8a66e19f68ccff5b4ba2c85ec4796083920e0d
tranthanh <tranvanthanhsd0@gmail.com>
2021-03-16 21:03:51 +0700

    feat:map - add range

--- ninja-level3/section10-map-introduction.go
+++ ninja-level3/section10-map-introduction.go
@@ -1,4 +1,4 @@
-package main
+papackage main
 
 import "fmt"
 
@@ -15,5 +15,26 @@ func main() {
 	fmt.Println(x["Van Thanh1"]) // Giá trị này sẽ = 0 vì chưa được khai báo
 
 	fmt.Println(x["Tran Thanh"])
+	
+	v, ok := x["Van Thanh1"] 
+	fmt.Println(v)
+	fmt.Println(ok)
+	
+	x["Van Thanh2"] = 44
+	
+	if v, ok := x["Van Thanh1"]; ok {
+		fmt.Println(v, ok)
 
+	}
+	
+	for k, v := range x {
+	fmt.Println(k, v)
+	}
+	
+	x1 := []int{1, 3, 5, 7, 10}
+	fmt.Println(x1)
+	
+	for i, v := range x1 {
+	fmt.Println(i, v)
+	}
 }
