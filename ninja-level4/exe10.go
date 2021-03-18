b206d9540d2f95af2921af6a34c2b48f5e20dc21
tranthanh <tranvanthanhsd0@gmail.com>
2021-03-18 21:15:48 +0700

    feat:dung ham delete

---
+++ ninja-level4/exe10.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := map[string][]string{
+		`bond_james`:      []string{`Shaken, not stirred`, `Martinis`, `Women`},
+		`moneypenny_miss`: []string{`James Bond`, `Literature`, `Computer Science`},
+		`no_dr`:           []string{`Being evil`, `Ice cream`, `Sunsets`},
+	}
+	fmt.Println(x)
+
+	x[`a`] = []string{`b`, `c`, `d`}
+	delete(x, `moneypenny_miss`)
+
+	for i, v := range x {
+		fmt.Println("This is record for", i)
+		for j, k := range v {
+			fmt.Println("\t", j, k)
+		}
+	}
+
+}
