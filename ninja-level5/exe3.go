b9deeb116635ac65d39df10df3562a66514bb550
tranthanh <tranvanthanhsd0@gmail.com>
2021-03-24 21:23:33 +0700

    feat:nhung trong struck

---
+++ ninja-level5/exe3.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+type vehicle struct {
+	doors int
+	color string
+}
+type truck struct {
+	vehicle
+	fourWheel bool
+}
+type sedan struct {
+	vehicle
+	luxury bool
+}
+
+func main() {
+
+	p1 := truck{
+		vehicle: vehicle{
+			doors: 2,
+			color: "white",
+		},
+		fourWheel: true,
+	}
+
+	p2 := sedan{
+		vehicle: vehicle{
+			doors: 3,
+			color: "black",
+		},
+		luxury: false,
+	}
+	fmt.Println(p1)
+	fmt.Println(p2)
+
+}
