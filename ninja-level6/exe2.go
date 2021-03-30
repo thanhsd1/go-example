cffed529ec6c47d9fa34652fb9963b847f07d17f
tranthanh <tranvanthanhsd0@gmail.com>
2021-03-30 20:10:22 +0700

    feat:add code

--- ninja-level6/exe2.go
+++ ninja-level6/exe2.go
@@ -8,6 +8,11 @@ func main() {
 	ii := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	n := foo(ii...)
 	fmt.Println(n)
+
+
+	ii2 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9,}
+	 n2 := bar(ii2)
+	fmt.Println(n2)
 }
 func foo(xi ...int) int {
 	total := 0
@@ -16,3 +21,9 @@ func foo(xi ...int) int {
 	}
 	return total
 }
+func bar(xi []int) int {
+	total := 0
+	for _, v := range xi{
+		total += v	
+	}
+	return total
