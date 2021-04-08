4b4dd102b0a85b8079844318ebe92131df887ad4
tranthanh <tranvanthanhsd0@gmail.com>
2021-04-08 21:34:09 +0700

    fix:fix person and add code

--- section18-application/140-sort-custom.go
+++ section18-application/140-sort-custom.go
@@ -2,20 +2,29 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
-type person struct {
-	first string
-	age   int
+type Person struct {
+	First string
+	Age   int
 }
 
+type ByAge []Person
+
+func (a ByAge) Len() int           { return len(a) }
+func (a ByAge) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByAge) Less(i, j int) bool { return a[i].Age < a[j].Age }
+
 func main() {
-	p1 := person{"TranThanh1", 24}
-	p2 := person{"TranThanh2", 25}
-	p3 := person{"TranThanh3", 26}
-	p4 := person{"TranThanh4", 27}
+	p1 := Person{"TranThanh1", 24}
+	p2 := Person{"TranThanh2", 15}
+	p3 := Person{"TranThanh3", 12}
+	p4 := Person{"TranThanh4", 16}
 
-	people := []person{p1, p2, p3, p4}
+	people := []Person{p1, p2, p3, p4}
 
+	fmt.Println(people)
+	sort.Sort(ByAge(people))
 	fmt.Println(people)
 }
