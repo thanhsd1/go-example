8746c6b5ebabfbf51a5d55c9e9b2c1026fe40790
tranthanh <tranvanthanhsd0@gmail.com>
2021-04-21 14:44:08 +0700

    review code

---
+++ ninja-level10/exe3.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main (){
+		c := gen()
+		receive(c)
+
+		fmt.Println("about to exit")
+}		
+func receive (<-chan int){
+		for v := range c {
+			fmt.Println(v)
+		}
+}		
+func gen (chan int){
+	gen := make(chan int)
+	go func (){
+		for i:= 0; i < 10; i++ {
+			c <- i
+	
+		} 
+	}()
+	close(c)
+
+}
+return c
+
+
+}
